4ffdb5d78da2dabd6a045ed1b953d33cb1c80354
Shinjice <Tdna84@gmail.com>
2021-01-19 17:55:13 +0100

    Testing reCAPTCHA v3

---
+++ SampleWeb/Models/SampleViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+
+namespace SampleWeb.Models
+{
+    public class SampleViewModel
+    {
+        [DisplayName("Test")]
+        public string Text { get; set; }
+    }
+}
